40ef3b575a44454fd9a570db990eeda91b998cb6
Patrick <sirpatrickcoleman@gmail.com>
2024-09-21 12:05:38 +1000

    updated to not be on localhost

--- SubscriptionApi.js
+++ SubscriptionApi.js
@@ -42,6 +42,6 @@ app.get('/unsubscribe/:email/:productid', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
